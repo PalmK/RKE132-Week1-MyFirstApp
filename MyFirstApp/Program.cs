20b007b0e0ae0ef7be09d5e54c3af95d643d7647
Kristjan Palm <kpalm@tktk.ee>
2023-02-11 08:43:30 +0200

    creat new app

---
+++ MyFirstApp/Program.cs
@@ -0,0 +1,13 @@
+﻿// This is comment to my code
+
+//rakendus küsib kasutajal sisesestada tema nime
+//rakendus tervitab kasutajat nimepidi
+
+Console.WriteLine("Enter your name"); //Output
+//string - sõne
+string userName = Console.ReadLine(); //Input
+
+Console.WriteLine("Hello" + ", " + userName + "!"); //Output
+
+//string in terpolation
+Console.WriteLine($"Hello, {userName} !"); //Output
